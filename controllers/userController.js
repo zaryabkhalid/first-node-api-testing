63f2bb7245a5742fbd12694177b98378331de738
Zaryab Khalid <ZaryabKhalid43@gmail,com>
2022-12-30 17:07:29 +0400

    bug fix

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,18 +1,18 @@
-import asyncHandler from "express-async-handler"
-import CustomErrors from "../errors/CustomErrors.js"
-import User from "../model/userModel.js"
-import bcrypt from "bcrypt"
-import generateJWT from "../utils/generateJWT.js"
+import asyncHandler from "express-async-handler";
+import CustomErrors from "../errors/CustomErrors.js";
+import User from "../model/userModel.js";
+import bcrypt from "bcrypt";
+import generateJWT from "../utils/generateJWT.js";
 
 // Register Controller
 export const createUser = asyncHandler(async (req, res, next) => {
-  const { firstname, lastname, username, email, password } = req.body
+  const { firstname, lastname, username, email, password } = req.body;
   if (!firstname || !lastname || !username || !email || !password) {
-    next(CustomErrors.validationError("All fields are required..."))
+    next(CustomErrors.validationError("All fields are required..."));
   }
-  const userExists = await User.exists({ username, email })
+  const userExists = await User.exists({ username, email });
   if (userExists) {
-    next(CustomErrors.validationError("User Already exists..."))
+    next(CustomErrors.validationError("User Already exists..."));
   } else {
     const user = await User.create({
       firstname,
@@ -20,7 +20,7 @@ export const createUser = asyncHandler(async (req, res, next) => {
       username,
       email,
       password,
-    })
+    });
     if (user) {
       res.status(201).json({
         message: "User registration successful",
@@ -32,22 +32,22 @@ export const createUser = asyncHandler(async (req, res, next) => {
           email: user.email,
           token: generateJWT(user._id),
         },
-      })
+      });
     } else {
-      next(CustomErrors.SomethingWentWrong())
+      next(CustomErrors.SomethingWentWrong());
     }
   }
-})
+});
 
 // Login Controller
 export const loginUser = asyncHandler(async (req, res, next) => {
-  const { username, password } = req.body
+  const { username, password } = req.body;
   if (!username || !password) {
     return next(
       CustomErrors.validationError("Username and password are required...")
-    )
+    );
   }
-  const user = await User.findOne({ username })
+  const user = await User.findOne({ username });
   if (user && (await bcrypt.compare(password, user.password))) {
     res.status(200).json({
       message: "Login Successful",
@@ -57,18 +57,18 @@ export const loginUser = asyncHandler(async (req, res, next) => {
         email: user.email,
         jwt: generateJWT(user._id),
       },
-    })
+    });
   } else {
-    return next(CustomErrors.validationError("Invalid user cradentials"))
+    return next(CustomErrors.validationError("Invalid user cradentials"));
   }
-})
+});
 
 // Get User Data
 export const getUsersData = asyncHandler(async (req, res, next) => {
-  const { id, username, email } = await User.findById(req.user._id)
+  const { id, username, email } = await User.findById(req.user._id);
   return res.status(200).json({
     id: id,
     name: username,
     email: email,
-  })
-})
+  });
+});
