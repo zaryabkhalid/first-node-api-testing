8e174d5b775ff50461d4256e0f628322e40afe31
Zaryab Khalid <zaryabkhalid43@gmail.com>
2023-03-30 08:13:22 +0500

    server file updated

--- controllers/blogController.js
+++ controllers/blogController.js
@@ -1,96 +1,96 @@
-import asyncHandler from "express-async-handler"
-import Blog from "../model/blogModel.js"
-import CustomErrors from "../errors/CustomErrors.js"
+import asyncHandler from "express-async-handler";
+import Blog from "../model/blogModel.js";
+import CustomErrors from "../errors/CustomErrors.js";
 
 /*
  * Get All blogs Controller function ****
  */
 export const getAllBlogs = asyncHandler(async (req, res, next) => {
-  const blogs = await Blog.find()
-  if (!blogs) {
-    next(CustomErrors.NotFoundError())
-  } else {
-    res.status(200).json(blogs)
-  }
-})
+    const blogs = await Blog.find();
+    if (!blogs) {
+        next(CustomErrors.NotFoundError());
+    } else {
+        res.status(200).json(blogs);
+    }
+});
 
 /*
  * Create a new blog Controller function ****
  */
 export const createBlog = asyncHandler(async (req, res, next) => {
-  const { title, description, image } = req.body
-  if (!title || !description || !image) {
-    next(CustomErrors.validationError())
-  }
-  const blog = new Blog({
-    title,
-    description,
-    image,
-    user: req.user.id,
-  })
-  if (blog) {
-    const newblog = await blog.save()
-    res.status(201).json({
-      Success: true,
-      message: "Blog created successfully...",
-      newblog,
-    })
-  } else {
-    next(CustomErrors.SomethingWentWrong("blog creation failed"))
-  }
-})
+    const { title, description, image } = req.body;
+    if (!title || !description || !image) {
+        next(CustomErrors.validationError());
+    }
+    const blog = new Blog({
+        title,
+        description,
+        image,
+        user: req.user.id,
+    });
+    if (blog) {
+        const newblog = await blog.save();
+        res.status(201).json({
+            Success: true,
+            message: "Blog created successfully...",
+            newblog,
+        });
+    } else {
+        next(CustomErrors.SomethingWentWrong("blog creation failed"));
+    }
+});
 
 /*
  * Delete Blog Controller function ****
  */
 export const deleteBlog = asyncHandler(async (req, res, next) => {
-  const filterBlog = await Blog.findById(req.params.id)
-  if (filterBlog.user._id.toString() !== req.user._id.toString()) {
-    next(CustomErrors.AuthorizationError("Deletion Access denied..."))
-  }
-  if (filterBlog) {
-    await filterBlog.remove()
-    res.status(200).json({
-      message: "blog deleted successfully",
-    })
-  } else {
-    next(CustomErrors.NotFoundError())
-  }
-})
+    const filterBlog = await Blog.findById(req.params.id);
+    if (filterBlog.user._id.toString() !== req.user._id.toString()) {
+        next(CustomErrors.AuthorizationError("Deletion Access denied..."));
+    }
+    if (filterBlog) {
+        await filterBlog.remove();
+        res.status(200).json({
+            message: "blog deleted successfully",
+        });
+    } else {
+        next(CustomErrors.NotFoundError());
+    }
+});
 
 /*
  * Filter Blog Controller function ****
  */
 export const findBlogById = asyncHandler(async (req, res, next) => {
-  const { id } = req.params
-  const blog = await Blog.findById(id)
-  if (blog) {
-    res.status(200).json(blog)
-  } else {
-    next(CustomErrors.NotFoundError("Blog not found..."))
-  }
-})
+    const { id } = req.params;
+    const blog = await Blog.findById(id);
+    if (blog) {
+        res.status(200).json(blog);
+    } else {
+        next(CustomErrors.NotFoundError("Blog not found..."));
+    }
+});
 
 /*
  * Update Blog Controller function ****
  */
 export const updateBlog = asyncHandler(async (req, res, next) => {
-  const { title, description, image } = req.body
-  const { id } = req.params
-  const blog = await Blog.findById(id)
-  if (blog.user.toString() !== req.user._id.toString()) {
-    next(CustomErrors.AuthorizationError())
-  }
-  if (blog) {
-    ;(blog.title = title),
-      (blog.description = description),
-      (blog.image = image)
-    const updatedblog = await blog.save()
-    res.status(200).json({
-      message: "Blog updated successfully...",
-      updatedblog,
-    })
-  } else {
-    next(CustomErrors.NotFoundError("Blog Not Found"))
-  }
-})
+    const { title, description, image } = req.body;
+    const { id } = req.params;
+    const blog = await Blog.findById(id);
+    if (blog.user.toString() !== req.user._id.toString()) {
+        next(CustomErrors.AuthorizationError());
+    }
+    if (blog) {
+        blog.title = title;
+        blog.description = description;
+        blog.image = image;
+        const updatedblog = await blog.save();
+        res.status(200).json({
+            message: "Blog updated successfully...",
+            updatedblog,
+        });
+    } else {
+        next(CustomErrors.NotFoundError("Blog Not Found"));
+    }
+});
--- server.js
+++ server.js
@@ -1,23 +1,23 @@
-import express, { json } from "express"
-import dotenv from "dotenv"
-import userRouter from "./routes/user.js"
-import erorrHandler from "./middleware/ErorrHandler.js"
-import connectDB from "./config/DatabaseConnect.js"
-import blogRouter from "./routes/blog.js"
+import express from "express";
+import dotenv from "dotenv";
+import userRouter from "./routes/user.js";
+import erorrHandler from "./middleware/ErorrHandler.js";
+import connectDB from "./config/DatabaseConnect.js";
+import blogRouter from "./routes/blog.js";
 
-const app = express()
-dotenv.config()
-const PORT = process.env.APP_PORT || 3500
-connectDB()
+const app = express();
+dotenv.config();
+const PORT = process.env.APP_PORT || 3500;
+connectDB();
 
-app.use(express.json())
-app.use("/api/v2", userRouter)
-app.use("/api/v2/blogs", blogRouter)
+app.use(express.json());
+app.use("/api/v2", userRouter);
+app.use("/api/v2/blogs", blogRouter);
 app.get("/", (req, res) => {
-  res.status(200).send("<h2>Hi i am live API Project Two</h2>")
-})
-app.use(erorrHandler)
+  res.status(200).send("<h2>Hi i am live API Project Two</h2>");
+});
+app.use(erorrHandler);
 
 app.listen(PORT, () => {
-  console.log(`Server is listening on ${PORT}`)
-})
+  console.log(`Server is listening on ${PORT}`);
+});
